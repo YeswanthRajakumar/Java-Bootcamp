fdea33ca8b6afeb8f7d52118c6ef69bfe7677a98
Yeswanth Rajakumar <yeswanthjayanthi@gmail.com>
2021-05-06 11:32:45 +0530

    Added some prints in Calculator

--- Day2/Calculator.java
+++ Day2/Calculator.java
@@ -1,5 +1,9 @@
 public class Calculator {
      public static void main(String[] args){
         System.out.println("completed Calculator.....");
+         System.out.println("completed Calculator.....");
+          System.out.println("completed Calculator.....");
+          System.out.println("completed Calculator.....");
+           System.out.println("updated  Calculator.....");
     }
 }
